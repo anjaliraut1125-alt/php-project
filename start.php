e9ed2c77028b051064515cdba0173f4f47859d70
anjaliraut1125-alt <anjaliraut1125@gmail.com>
2026-03-14 12:06:27 +0530

    PHP File

---
+++ start.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP START [14-03-2026]</h1>
+    
+    <?php
+
+     echo "Hello PHP [14-03-2026]<br><br>";
+     echo "PHP  is stand for [Hypertext Preproceesor]<br><br>";
+     echo "I am a server-side scripting language"   
+
+    ?>
+
+
+</body>
+</html>
+
+

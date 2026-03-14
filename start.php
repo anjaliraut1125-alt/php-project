64ccd5b27bc22bcf9e9ad125c78b4be6e46e61ae
anjaliraut1125-alt <anjaliraut1125@gmail.com>
2026-03-14 14:07:52 +0530

    add php new

--- start.php
+++ start.php
@@ -12,7 +12,8 @@
 
      echo "Hello PHP [14-03-2026]<br><br>";
      echo "PHP  is stand for [Hypertext Preproceesor]<br><br>";
-     echo "I am a server-side scripting language"   
+     echo "I am a server-side scripting language" 
+     echo "Welcome to SIT"  
 
     ?>
 
